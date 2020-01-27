e54339e6e9e3869f6ad583fc0074218c8a72cc6c
peter kuria <you@example.com>
2020-01-27 04:38:53 -0500

    added bootstrp

--- news/views.py
+++ news/views.py
@@ -3,40 +3,39 @@ from django.http import HttpResponse,Http404
 import datetime as dt
 
 # Create your views here.
-def welcome(request):
-    return HttpResponse('hello brother')
+#def welcome(request):
+    #return HttpResponse('hello brother')
 
-def convert_dates(dates):
+#def convert_dates(dates):
     #function that gets the weekly news_of_day
-    day_number=dt.date.weekday(dates)
-    days=['Monday','Tuesday','Wednesday','Thursday','Saturday',"Sunday"]
+    #day_number=dt.date.weekday(dates)
+    #days=['Monday','Tuesday','Wednesday','Thursday','Saturday',"Sunday"]
     #returning the actual day of the weekly
-    day=days[day_number]
+    #day=days[day_number]
 
-    return day
+    #return day
 
 def news_of_day(request):
-    date=dt.date.today()
-    day = convert_dates(date)
-    html=f'''
-            <html>
-               <body>
-                   <h1>News for {day} {date.day}-{date.month}-{date.year}</h1>
-            </html>
-                '''
-    return HttpResponse(html)
+    date = dt.date.today()
+    return render(request, 'all-news/today-news.html', {"date": date,})
+
+
 
 def past_days_news(request,past_date):
-        try:
-            # Converts data from the string Url
-            date = dt.datetime.strptime(past_date,'%Y-%m-%d').date()
-        except ValueError:
-        day = convert_dates(date)
-        html = f'''
-            <html>
-                <body>
-                    <h1>News for {day} {date.day}-{date.month}-{date.year}</h1>
-                </body>
-            </html>
-                '''
-    return HttpResponse(html)
+          try:
+             # Converts data from the string Url
+             date = dt.datetime.strptime(past_date, '%Y-%m-%d').date()
+
+          except ValueError:
+            # Raise 404 error when ValueError is thrown
+            raise Http404()
+            assert False
+
+          if date == dt.date.today():
+            return redirect(news_of_day)
+
+          return render(request, 'all-news/past-news.html', {"date": date})
+
+
+def welcome(request):
+    return render(request,'welcome.html')
--- tribune/settings.py
+++ tribune/settings.py
@@ -31,6 +31,9 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'news',
+    'bootstrap3',
+    'news.apps.NewsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',

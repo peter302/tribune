59f7b0550aca59e162e1bec8104d57169165151a
peter kuria <you@example.com>
2020-01-27 02:56:29 -0500

    added a function to get the day of the week

--- news/views.py
+++ news/views.py
@@ -9,14 +9,19 @@ def welcome(request):
 def convert_dates(dates):
     #function that gets the weekly news_of_day
     day_number=dt.date.weekday(dates)
-     days=['Monday','Tuesday','Wednesday','Thursday','Saturday',"Sunday"]    
+    days=['Monday','Tuesday','Wednesday','Thursday','Saturday',"Sunday"]
+    #returning the actual day of the weekly
+    day=days[daynumber]
+
+    return day
 
 def news_of_day(request):
     date=dt.date.today()
+    day = convert_dates(date)
     html=f'''
             <html>
                <body>
-                   <h1>{date.day}-{date.month}-{date.year}</h1>
+                   <h1>News for {day} {date.day}-{date.month}-{date.year}</h1>
             </html>
                 '''
     return HttpResponse(html)

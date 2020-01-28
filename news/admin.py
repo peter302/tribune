316e0a78d14b8284aa558afec5c1ecdb1d67b800
peter kuria <you@example.com>
2020-01-28 02:47:25 -0500

    finished

--- news/admin.py
+++ news/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Editor,Article,tags
+
+class ArticleAdmin(admin.ModelAdmin):
+    filter_horizontal =('tags',)
+    
+admin.site.register(Editor)
+admin.site.register(Article)
+admin.site.register(tags)

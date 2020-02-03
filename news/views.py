0169ec9ca38066453ff3d7c15129263ec733bbb3
peter kuria <you@example.com>
2020-02-03 00:22:16 -0500

    aded a form valodation functionality

--- news/views.py
+++ news/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse,Http404
 import datetime as dt
 from .models import Article
+from .forms import NewsLetterForm
 
 # Create your views here.
 #def welcome(request):
@@ -19,7 +20,14 @@ from .models import Article
 def news_of_day(request):
     date = dt.date.today()
     news = Article.todays_news()
-    return render(request, 'all-news/today-news.html', {"date": date,"news":news})
+
+    if request.method == 'POST':
+        form = NewsLetterForm(request.POST)
+        if form.is_valid():
+            print('valid')
+    else:
+        form = NewsLetterForm()
+    return render(request, 'all-news/today-news.html', {"date": date,"news":news,"letterForm":form})
 
 
 
